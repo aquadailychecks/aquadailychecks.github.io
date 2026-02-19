e18e3c64a4a8bbe5d7248bbcc7bfe08d06281f8b
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2026-02-19 06:23:24 -0600

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYeIrEFJ0AIK2EScA91lPQXtqf-lOfBWk",
+  authDomain: "aquadailychecks.firebaseapp.com",
+  projectId: "aquadailychecks",
+  storageBucket: "aquadailychecks.firebasestorage.app",
+  messagingSenderId: "1088043283120",
+  appId: "1:1088043283120:web:d23dedd97b07c041e7c175",
+  measurementId: "G-BM64K8XYV0"
+};
